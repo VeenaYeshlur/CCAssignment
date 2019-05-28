36e1674e4b85d4244a002e43e03a9fcce06ff1ab
VeenaYeshlur <48153920+VeenaYeshlur@users.noreply.github.com>
2019-05-28 18:04:45 +0100

    Update Airport.java

--- CloudComputing/src/Airline/Airport.java
+++ CloudComputing/src/Airline/Airport.java
@@ -1,23 +1,26 @@
 package Airline;
 
 /***************************************************************************
- * Airport: returns latitude and longitude
+ * Airport: returns Code, Latitude and Longitude
  ***************************************************************************/
 
 public class Airport {
 	String code;
 	double lat, lon;
 	
+	// constructor to return Airport Details
 	Airport(String _code, String _lat, String _lon){
 		code = _code;
 		lat = Double.parseDouble(_lat);
 		lon = Double.parseDouble(_lon);
 	}
-		
+	
+	//Get Latitude
 	public double getLatitude(){
 		return lat;
 	}
 	
+	//Get Longitude
 	public double getLongitude(){
 		return lon;
 	}
@@ -25,6 +28,7 @@ public class Airport {
 		return code + " " + lat + " " + lon;
 	}
 	
+	//Get Airport Code
 	public String getCode(){
 		return code;
 	}
